89b409aa1985c4b75f328184e0dfa72cb61b6a51
honkingpill <116879009+honkingpill@users.noreply.github.com>
2025-12-11 17:52:02 +0000

    install script with my often used pkg
    For hyprland build

---
+++ gay-install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# ==========
+# Here's some of my most-used packages
+# ==========
+PACKAGES=(
+    	"networkmanager"
+    	"cifs-utils"
+    	"xorg"
+	"sddm"
+	"ttf-droid"
+	"curl"
+	"ttf-dejavu"
+    	"ttf-liberation"
+   	"ttf-ubuntu-font-family"
+    	"noto-fonts"
+    	"noto-fonts-cjk"
+    	"noto-fonts-emoji"
+	"noto-fonts-extra"
+    	"ttf-opensans"
+    	"ttf-jetbrains-mono-nerd"
+	"ttf-hack"
+	"git"
+	"thunar"
+	"kitty"
+	"hyprlock"
+	"hyprland"
+	"waybar"
+	"firefox"
+	"rofi"
+	"telegram-desktop"
+	"steam"
+	"nerd-fonts-complete"
+)
+
+
+if [[ $EUID -ne 0 ]]; then
+	echo "Script need to run as a root. Use Sudo"
+    exit 1
+fi
+
+pacman -Sy
+
+for pkg in "${PACKAGES[@]}"; do
+    if pacman -Si "$pkg" &>/dev/null; then
+        echo "Syncing: $pkg"
+        pacman -S --noconfirm --needed "$pkg"
+    else
+        echo "$pkg not avaible, check for pacman.d file"
+    fi
+done
+
+fc-cache -fv
+
+echo "Here we go again"
